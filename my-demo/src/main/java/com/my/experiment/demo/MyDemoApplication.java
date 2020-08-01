86ae60b99f188e1fc52a157218ad719a5da9fc39
dong1ding <dong1ding@126.com>
2020-08-01 21:08:17 +0800

    test

--- my-demo/src/main/java/com/my/experiment/demo/MyDemoApplication.java
+++ my-demo/src/main/java/com/my/experiment/demo/MyDemoApplication.java
@@ -3,6 +3,9 @@ package com.my.experiment.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 主启动类
+ */
 @SpringBootApplication
 public class MyDemoApplication {
     public static void main(String[] args) {
