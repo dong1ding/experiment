9a0c1d85d82153d7bbc2dc604049de053e01296f
dong1ding <dong1ding@126.com>
2020-08-01 19:46:27 +0800

    initial commit

---
+++ my-demo/src/test/java/com/my/experiment/demo/MyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.experiment.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MyDemoApplicationTests {
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
